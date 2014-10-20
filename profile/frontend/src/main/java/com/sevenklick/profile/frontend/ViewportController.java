89ef857bd959f517b3855cc0931bfd313bc1d463
Pierre Petersson <pierre.petersson@primelog.com>
2014-10-20 22:22:46 +0200

    Fileupload implementation started

--- profile/frontend/src/main/java/com/sevenklick/profile/frontend/ViewportController.java
+++ profile/frontend/src/main/java/com/sevenklick/profile/frontend/ViewportController.java
@@ -7,9 +7,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -31,6 +37,41 @@ public class ViewportController extends BaseController {
     @RequestMapping(value = "/secured/template/profile")
     public String profileViewTemplate() throws IOException {
         return "templates/profile";
+    }
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public
+    String uploadFileHandler(@RequestParam("file") MultipartFile file) {
+
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + file.getOriginalFilename());
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+
+                System.out.println("Server File Location="
+
+                        + serverFile.getAbsolutePath());
+
+                return "templates/profile";
+            } catch (Exception e) {
+                return "You failed to upload " + "kk" + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + "kk"
+                    + " because the file was empty.";
+        }
     }
 
 }
